ed20d7a74c312ef557720ca6f435d4cf969faefa
dysignin_ <77549201+audyalvasyha@users.noreply.github.com>
2025-02-01 03:48:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -68,6 +68,18 @@
     </div>
 
     <script src="js/scriptFront.js"></script>
+    <script>
+        if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+}
+
+    </script>
 </body>
 
 </html>
